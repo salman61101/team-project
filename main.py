af150e8ac1d4441e9ad7ecf332bd551a77cea3b1
salman61101 <muhammadsalman5248@gmail.com>
2025-09-25 11:43:39 -0400

    hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 # Simple program for SSDD assignment - prints environment info (beginner-friendly)
 import platform
-print("Hello from:", "Your Name or Roll")
+print("Hello from:", "Salman")
 print("Platform:", platform.platform())
+print:("hello hello")
