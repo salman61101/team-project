4e411f7b83a4521994654427219a07d0de892e26
salman61101 <muhammadsalman5248@gmail.com>
2025-09-25 11:33:23 -0400

    Added main.py with initial code

---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+# Simple program for SSDD assignment - prints environment info (beginner-friendly)
+import platform
+print("Hello from:", "Your Name or Roll")
+print("Platform:", platform.platform())
